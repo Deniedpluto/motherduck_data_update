79d107700a727e8ffd28702e37e6c325a446855d
Peter Matson <Matson.peter@gmail.com>
2025-01-08 21:43:14 -0800

    Update script.sql
    Added new lines to updated commander deck tags

--- script.sql
+++ script.sql
@@ -17,3 +17,7 @@ VALUES ('github action', CURRENT_TIMESTAMP);
 -- refresh espresso data
 CREATE OR REPLACE TABLE PM.Espresso_Data AS
 FROM read_csv('https://docs.google.com/spreadsheets/d/1fD2Tozfvnt6u70lbeQN5FLM1ocHxheAmWKMH6B_oQXs/export?format=csv&gid=1056545167');
+
+-- refresh commander tags
+CREATE OR REPLACE TABLE MTG.CommanderDeckTags AS
+FROM read_csv('https://docs.google.com/spreadsheets/d/1SqxtkeIBL_w4j77IXBFqOFWPuRLPnvy7G_BMkYnnDAM/export?format=csv&gid=0');
